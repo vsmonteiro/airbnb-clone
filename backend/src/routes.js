ba0deb00aba642c542e734ed549b079e0788b3bf
Vinicius Monteiro <vmonteiro@gvdasa.com.br>
2019-10-03 13:45:47 -0300

    Conclusão da api

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,12 +3,18 @@ const multer = require('multer');
 const uploadConfig = require('./config/Upload');
 
 const SessionController = require('./controllers/SessionController');
+const DashBoardController = require('./controllers/DashBoardController');
+const BookingController = require('./controllers/BookingController');
 const SpotController = require('./controllers/SpotController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
 
+routes.get('/spots', SpotController.index);
+routes.get('/dashboard', DashBoardController.show);
+
 routes.post('/sessions', SessionController.store);
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 
 module.exports = routes;
